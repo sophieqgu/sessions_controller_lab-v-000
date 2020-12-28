57e96d56832f1fcbb22d892e6deb4b16e59737cf
Sophie Gu <sophieqgu@gmail.com>
2020-12-28 02:34:38 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,10 @@ class SessionsController < ApplicationController
 
   def create
     session[:name] = params[:name]
+    redirect_to root_path
   end
 
   def destroy
     session.delete :name
-  end 
+  end
 end
