e3c495b15d14761695a2e5e953d48636a31df735
Sophie Gu <sophieqgu@gmail.com>
2020-12-28 02:29:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def hello
+    if session[:name]
+      render :hello
+    else
+      link_to
+    end 
+  end
+
 end
